49ac827283df613c3936a7851cf439140bf963a0
malcolm-smith <malcolm.henry.smith@gmail.com>
2018-11-09 14:00:22 -0500

    added solution #12

--- Project Euler/PE Problem 12 - Highly Divisible Triangular Number/src/FooBar12.java
+++ Project Euler/PE Problem 12 - Highly Divisible Triangular Number/src/FooBar12.java
@@ -1,7 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class FooBar12 {
-	
+
 	public static void main(String[] args) {
+		int divisors = 0;
+		int term = 1;
+		int a = 0;
 		
+		while (divisors <= 500) {
+			term++;
+			a = 0;
+			for (int i = 0; i < term; i++) {
+				a += i;
+			}
+			divisors = allFactors(a).size();
+		}
+		System.out.println(a);
+	}
+
+	public static ArrayList<Integer> allFactors(int a) {
+
+		int upperlimit = (int) (Math.sqrt(a));
+		ArrayList<Integer> factors = new ArrayList<Integer>();
+		for (int i = 1; i <= upperlimit; i += 1) {
+			if (a % i == 0) {
+				factors.add(i);
+				if (i != a / i) {
+					factors.add(a / i);
+				}
+			}
+		}
+		Collections.sort(factors);
+		return factors;
 	}
 }
