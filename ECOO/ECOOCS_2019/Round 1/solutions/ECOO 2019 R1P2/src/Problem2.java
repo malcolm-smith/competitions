9f71fad56cf67b7490b402cd90688de803befb55
malcolm-smith <malcolm.henry.smith@gmail.com>
2019-04-15 08:53:06 -0400

    Added solution files

---
+++ ECOO/ECOOCS_2019/Round 1/solutions/ECOO 2019 R1P2/src/Problem2.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class Problem2 {
+
+	private static final int NUM_OF_TEST_CASES = 10; //or whatever number of test cases are in the file
+	private static String filePath = "../../../data/DATA20.txt"; //write the file path here
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner in = new Scanner(new File(filePath));
+		for (int i = 0; i < NUM_OF_TEST_CASES; i++) {
+			new Problem2().run(in);
+		}
+		in.close();
+	}
+
+	public void run(Scanner sc) {
+
+	}
+}
